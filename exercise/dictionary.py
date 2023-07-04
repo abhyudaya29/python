01b327a3ab9be973a86ad85f87282b053398a454
abhyudaya29 <abhyudayadev123@gmail.com>
2023-07-04 13:56:36 +0530

    Update dictionary.py

--- exercise/dictionary.py
+++ exercise/dictionary.py
@@ -3,8 +3,15 @@ myword = {"centos": "Open source server distribution.",
         "redhat": "Propriatory OS.",
         "arch": "Open Source desktop and client distribution."
         }
-print(myword.keys())
-print("Check Keys name from above line.")
-print("Enter your choice name:", end='')
-tia1 = input()
-print(myword[tia1])
+# print(myword.keys())
+# print("Check Keys name from above line.")
+# print("Enter your choice name:", end='')
+# tia1 = input()
+# print(myword[tia1])
+
+
+
+
+dict_world={"Customer_name":"Abhyudaya",
+            "customer_id":1221}
+print(dict_world)
